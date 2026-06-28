5a1fe166b108daf80fe76807b88af8bb2d5ee11d
anantkpatel <imagine231216@gmail.com>
2026-06-28 16:22:19 +0530

    task add

--- Todo/src/App.jsx
+++ Todo/src/App.jsx
@@ -7,7 +7,11 @@ function App() {
 
   return (
     <>
-     
+     <h1 className="text-4xl font-extrabold !text-blue-600 border-b-2 p-2  ">Todos App 📕</h1>
+     <div className="input p-4 rounded-lg">
+      <input type = "text" placeholder="write a task" className = "border-2 border-gray-300 p-2 rounded-lg w-80 m-2 "></input>
+      <button className = "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-30 m-auto">Add</button>
+     </div>
     
     </>
   )
