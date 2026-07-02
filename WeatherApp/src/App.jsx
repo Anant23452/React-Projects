35a640a9a1091697d0a1a1597ea4ee621a8e2bb9
anantkpatel <imagine231216@gmail.com>
2026-07-02 20:23:30 +0530

    l

--- WeatherApp/src/App.jsx
+++ WeatherApp/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
      <h1 className="font-bold bg-blue-500">Weather App🌤️</h1>
-     <div className="body relative bg-gray-600 h-screen ">
+     <div className="body relative  h-screen bg-gradient-to-r from-blue-500 to-purple-600 ">
       <div className="search border-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-800 border-b-2 ">
          <input type="text" placeholder='search location' label="Search Location" className="relative bg-white p-3 my-2 rounded-md border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500  z-10 " />
          <button className="bg-blue-500 text-white p-3 rounded-md hover:bg-blue-600">Search</button>
