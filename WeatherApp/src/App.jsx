6ad5028d07c82ae38355db0e3d34902b13b96f7e
anantkpatel <imagine231216@gmail.com>
2026-07-02 20:34:56 +0530

    conversion

--- WeatherApp/src/App.jsx
+++ WeatherApp/src/App.jsx
@@ -10,10 +10,22 @@ function App() {
     <>
      <h1 className="font-bold bg-blue-500">Weather App🌤️</h1>
      <div className="body relative  h-screen bg-gradient-to-tr from-grey-800 to-purple-700 ">
+
+
+      {/* //search box */}
+
       <div className="search border-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-800 border-b-2 ">
          <input type="text" placeholder='search location' label="Search Location" className="relative bg-white p-3 my-2 rounded-md border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500  z-10 " />
          <button className="bg-blue-500 text-white p-3 rounded-md hover:bg-blue-600">Search</button>
       </div>
+
+      {/* //weather conversion  */}
+      <div className="weather-conversion  my-2 flex justify-center items-center gap-2">
+        <button className="bg-blue-500 text-white p-2 px-4 rounded-md hover:bg-blue-600">°C</button>
+        <button className="bg-blue-500 text-white p-2 px-4 rounded-md hover:bg-blue-600">°F</button>
+      </div>
+
+
      </div>
     </>
   )
