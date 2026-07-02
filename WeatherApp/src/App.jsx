b165b7efa4ca1acc77c76098fbe9dcc011a3deec
anantkpatel <imagine231216@gmail.com>
2026-07-02 17:43:52 +0530

    /m

--- WeatherApp/src/App.jsx
+++ WeatherApp/src/App.jsx
@@ -1,12 +1,20 @@
 import { useState } from 'react'
 import './App.css'
 
+      
+
 function App() {
   
 
   return (
     <>
-     <h1>Weather App</h1>
+     <h1 className="font-bold bg-blue-500">Weather App🌤️</h1>
+     <div className="body relative bg-gray-600 h-screen ">
+      <div className="search border-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-800 border-b-2 ">
+         <input type="text" placeholder='search location' label="Search Location" className="relative bg-white p-3 my-2 rounded-md border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500  z-10 " />
+         <button className="bg-blue-500 text-white p-3 rounded-md hover:bg-blue-600">Search</button>
+      </div>
+     </div>
     </>
   )
 }
