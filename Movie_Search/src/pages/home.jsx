03b583675f53b0e39e2246d76aa60867b6dbe6fa
anantkpatel <imagine231216@gmail.com>
2026-07-05 06:31:16 +0530

    o

--- Movie_Search/src/pages/home.jsx
+++ Movie_Search/src/pages/home.jsx
@@ -4,9 +4,13 @@ import{useState,useEffect} from 'react'
 function home() {
     const [search,setSearch]=useState("");
   return (
-    <form onSubmit={(e)=>{e.preventDefault()}}>
+    <form onSubmit={(e)=>{e.preventDefault()}
+    }
+     className="bg-gray-600 m-4"
+    >
      <input type="text"placeholder="search for a movie" value={search} onChange={(e)=>setSearch(e.target.value)}
-     className="bg-gray-200 border border-gray-300 text-gray-700 placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500"/>
+     className="border border-gray-400 p-2 rounded border border-gray-300 text-gray-700 placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500  "
+    />
      <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
        Search
      </button>
