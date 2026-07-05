d48a6708d80ff635ddc9530e595ca7aa3aaab4c1
anantkpatel <imagine231216@gmail.com>
2026-07-05 07:24:14 +0530

    Refactor home component layout and styling for improved structure

--- Movie_Search/src/pages/home.jsx
+++ Movie_Search/src/pages/home.jsx
@@ -4,17 +4,19 @@ import{useState,useEffect} from 'react'
 function home() {
     const [search,setSearch]=useState("");
   return (
-    <form onSubmit={(e)=>{e.preventDefault()}
+  <div className="home-parent mt-14 px-4">
+      <form onSubmit={(e)=>{e.preventDefault()}
     }
-     className="bg-gray-600 mt-14"
+     className="bg-gray-600 "
     >
      <input type="text"placeholder="search for a movie" value={search} onChange={(e)=>setSearch(e.target.value)}
-     className="border border-gray-400 p-2 rounded border border-gray-300 text-gray-700 placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500  "
+     className="border border-gray-400 p-2 rounded border border-gray-300 text-gray-700 placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500  " 
     />
      <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
        Search
      </button>
     </form>
+  </div>
   )
 }
 
