cfadccdc80736c9db308484410dd0b8735a10b2e
anantkpatel <imagine231216@gmail.com>
2026-07-06 12:19:05 +0530

    f

--- Movie_Search/src/pages/movieDetail.jsx
+++ Movie_Search/src/pages/movieDetail.jsx
@@ -39,11 +39,28 @@ function MovieDetail() {
       setLoading(false)
     }
   }
+  if(loading){
+    return <h1>Loading👀👀</h1>
+  }
+  if(error){
+    return <h2>{error}</h2>
+  }
+  if(!movie){
+    return <h2>Movie not found</h2>
+  }
 }
-console.log(id);
-return (
-  <div>
 
+return (
+  <div className="max-w-5xl mx-auto p-6" >
+    <img src={movie.Title} alt={movie.Poster} className='w-72 rounded' />
+    <h1>{movie.Title}</h1>
+    <p><strong>Year</strong>:{movie.Year}</p>
+    
+    <p><strong>Director:</strong> {movie.Director}</p>
+      <p><strong>Actors:</strong> {movie.Actors}</p>
+       <p><strong>IMDb:</strong> ⭐ {movie.imdbRating}</p>
+       
+    <p>{movie.Plot}</p>
   </div>
 )
 
