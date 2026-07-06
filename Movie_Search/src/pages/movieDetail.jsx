6ca7890e124e3edcd4b1655dd48ed5a883622dc7
anantkpatel <imagine231216@gmail.com>
2026-07-06 11:40:33 +0530

    try

--- Movie_Search/src/pages/movieDetail.jsx
+++ Movie_Search/src/pages/movieDetail.jsx
@@ -3,25 +3,40 @@ import { useParams } from 'react-router-dom'
 
 
 function MovieDetail() {
-  const [movie,setMovie]=useState(null)
-  const {id} = useParams();
-  function fetchMovie(id){
-  const response = await fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${search}`,)
-  const data= await response.json();
-  if(data.response===true){
-    setMovie(data.id)
+  const [movie, setMovie] = useState(null)
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const { id } = useParams();
+  function fetchMovie(id) {
+
+
+    setLoading(true);
+    setError("");
+
+    
+    try {
+      const response = await fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${search}`,)
+      const data = await response.json();
+      if (data.response === true) {
+        setMovie(data.id)
+      }
+      else {
+        setMovie(null);
+        setError("data not found")
+      }
+    } catch (error) {
+      setError("Fail to Fetch Movies")
+    }finally{
+      setLoading(false)
+    }
   }
-  else{
-    setMovie(null);
-    setError("data not found")
-  }
-  }
-  console.log(id);
-  return (
-    <div>
-      
-    </div>
-  )
+}
+console.log(id);
+return (
+  <div>
+
+  </div>
+)
 }
 
 export default MovieDetail
