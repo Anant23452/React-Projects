23eb4861e653f22f9c8f462a69dbeb5e53c67168
anantkpatel <imagine231216@gmail.com>
2026-07-08 08:34:27 +0530

    c

--- Github-profile/src/App.jsx
+++ Github-profile/src/App.jsx
@@ -15,7 +15,14 @@ function App() {
 
   return (
     <>
-    anant
+    <div className="main">
+      <h1>Github profile fetch</h1>
+      <form action="submit">
+        <input type="text" value={username} onChange={(e)=>e.target.value} />
+        <button className='bg-blue-600 hover:bg-blue-500 p-2 rounded'>search</button>
+      </form>
+    </div>
+    
     </>
   )
 }
