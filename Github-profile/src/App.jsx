f9c04f56ab13b73ceb408654b409531e40060987
anantkpatel <imagine231216@gmail.com>
2026-07-08 08:55:42 +0530

    g

--- Github-profile/src/App.jsx
+++ Github-profile/src/App.jsx
@@ -5,7 +5,10 @@ import './App.css';
 
 
 function App() {
-  const[username,setUsername]=useState("")
+  const[username,setUsername]=useState("");
+  // fetching data in form of object 
+  const [user, setUser] = useState(null);
+  // if object not found then it is in null state 
 
   
 
@@ -17,9 +20,21 @@ function App() {
     <>
     <div className="main">
       <h1>Github profile fetch</h1>
-      <form action="submit">
-        <input type="text" value={username} onChange={(e)=>e.target.value} />
-        <button className='bg-blue-600 hover:bg-blue-500 p-2 rounded'>search</button>
+      <form 
+      action="submit"
+       className=' flex  gap-4 justify-center items-center '
+       onSubmit={(e)=>{
+        e.preventDefault();
+        //function
+       }}
+       >
+        <input 
+         value={username}
+          onChange={(e)=>e.target.value}
+           className='border-2 border-gray-700 hover:border-gray-400 p-2 rounded-xl w-74'
+           placeholder='username'
+            />
+        <button className='bg-blue-600 hover:bg-blue-500 px-2 py-1 rounded-xl'>search</button>
       </form>
     </div>
     
