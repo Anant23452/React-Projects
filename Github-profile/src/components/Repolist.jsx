b82f94577b586e991484efe94273e07e6ff8dd10
anantkpatel <imagine231216@gmail.com>
2026-07-09 07:30:46 +0530

    v

--- Github-profile/src/components/Repolist.jsx
+++ Github-profile/src/components/Repolist.jsx
@@ -3,9 +3,9 @@ import React from "react";
 import Repoitems from "./Repoitems";
 
 export default function Repolist({ repos }) {
-  let [sort, setSort] = useState(" ");
-  let sortedlist = [...repos];
-  if (sortType === "stars") {
+   const [sortType, setSortType] = useState("most-stars");
+  let sortedRepos = [...repos];
+  if (sortType === "most-stars") {
     sortedRepos.sort((a, b) => b.stargazers_count - a.stargazers_count);
   } else if (sortType === "newest") {
     sortedRepos.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
@@ -18,16 +18,22 @@ export default function Repolist({ repos }) {
       <>
         <h2 className="text-bold">All Repository</h2>
         <div className="grid grid-cols-3 gap-4 w-full max-w-md mx-auto">
-          <button className="bg-yellow-500 text-white p-2 rounded">
+          <button
+           onClick ={()=>setSortType("More-star")}
+           className="bg-yellow-700 hover:bg-yellow-500 text-white p-2 rounded">
             More-star
           </button>
-          <button className="bg-slate-500 text-white p-2 rounded">
+          <button
+          onClick={()=>setSortType("Newest")}
+           className="bg-slate-700 hover:bg-slate-500 text-white p-2 rounded">
             Newest
           </button>
-          <button className="bg-red-500 text-white p-2 rounded">oldest</button>
+          <button
+          onClick={()=>setSortType("oldest")}
+           className="bg-red-700 hover:bg-red-500 text-white p-2 rounded">oldest</button>
         </div>
         <div className="repo  w-full h-full grid  grid-cols-2 md:grid-cols-3 gap-3 p-5 md:p-3">
-          {sortedlist.map((repo) => (
+          {sortedRepos.map((repo) => (
             <Repoitems repo={repo} />
           ))}
         </div>
