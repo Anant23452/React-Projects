53ad38b87269d244cbc19224a95d742f21b08377
anantkpatel <imagine231216@gmail.com>
2026-07-09 07:35:54 +0530

    v

--- Github-profile/src/components/Repolist.jsx
+++ Github-profile/src/components/Repolist.jsx
@@ -3,10 +3,10 @@ import React from "react";
 import Repoitems from "./Repoitems";
 
 export default function Repolist({ repos }) {
-   const [sortType, setSortType] = useState("most-stars");
-   const [currPage,setCurrPage]=useState(1);
-   const lastRepoIndex = currentPage * repoPerPage;
-const firstRepoIndex = lastRepoIndex - repoPerPage;
+  const [sortType, setSortType] = useState("most-stars");
+  const [currPage, setCurrPage] = useState(1);
+  const lastRepoIndex = currentPage * repoPerPage;
+  const firstRepoIndex = lastRepoIndex - repoPerPage;
   let sortedRepos = [...repos];
   if (sortType === "most-stars") {
     sortedRepos.sort((a, b) => b.stargazers_count - a.stargazers_count);
@@ -22,18 +22,23 @@ const firstRepoIndex = lastRepoIndex - repoPerPage;
         <h2 className="text-bold">All Repository</h2>
         <div className="grid grid-cols-3 gap-4 w-full max-w-md mx-auto">
           <button
-           onClick ={()=>setSortType("More-star")}
-           className="bg-yellow-700 hover:bg-yellow-500 text-white p-2 rounded">
+            onClick={() => setSortType("More-star")}
+            className="bg-yellow-700 hover:bg-yellow-500 text-white p-2 rounded"
+          >
             More-star
           </button>
           <button
-          onClick={()=>setSortType("Newest")}
-           className="bg-slate-700 hover:bg-slate-500 text-white p-2 rounded">
+            onClick={() => setSortType("Newest")}
+            className="bg-slate-700 hover:bg-slate-500 text-white p-2 rounded"
+          >
             Newest
           </button>
           <button
-          onClick={()=>setSortType("oldest")}
-           className="bg-red-700 hover:bg-red-500 text-white p-2 rounded">oldest</button>
+            onClick={() => setSortType("oldest")}
+            className="bg-red-700 hover:bg-red-500 text-white p-2 rounded"
+          >
+            oldest
+          </button>
         </div>
         <div className="repo  w-full h-full grid  grid-cols-2 md:grid-cols-3 gap-3 p-5 md:p-3">
           {sortedRepos.map((repo) => (
